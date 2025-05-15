f3052e77ff280cd437ff82bfa4133772cbda0c05
Vishwa <vnaik@smartsight.in>
2025-05-15 13:27:27 +0530

    minor ui changes and voice asistant added

--- dev-dist/sw.js
+++ dev-dist/sw.js
@@ -82,7 +82,7 @@ define(['./workbox-d9a5ed57'], (function (workbox) { 'use strict';
     "revision": "3ca0b8505b4bec776b69afdba2768812"
   }, {
     "url": "index.html",
-    "revision": "0.lfb9nqkt7qg"
+    "revision": "0.edtgdg19vrg"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
