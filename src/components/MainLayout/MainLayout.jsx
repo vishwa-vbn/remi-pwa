3f153177f973f931a5958fa1eb0830a1a6ce0ce0
Vishwa <vnaik@smartsight.in>
2025-05-20 15:27:01 +0530

    height changes

--- src/components/MainLayout/MainLayout.jsx
+++ src/components/MainLayout/MainLayout.jsx
@@ -1,3 +1,38 @@
+// import React from "react";
+// import { bindActionCreators } from "redux";
+// import { connect } from "react-redux";
+// import Header from "../../common/header";
+// import BottomNavigation from "../../common/BottomNavigation";
+// import "./MainLayout.css";
+// import VoiceAssistant from "../../common/VoiceAssistant";
+
+// class MainLayout extends React.Component {
+//   componentDidMount() {}
+
+//   render() {
+//     return (
+//       <div className="min-h-screen h-screen w-full flex flex-col bg-gray-100">
+//         <VoiceAssistant/>
+//         {window.location.pathname !== "/settings" ? (
+//           <Header className="flex-none" style={{ height: "10%" }} />
+//         ) : null}
+//         <main
+//           className="w-full overflow-auto scrollbar-hide"
+//           style={{  height: "84%" }}
+//         >
+//           {this.props.children}
+//         </main>
+//         <BottomNavigation className="flex-none"  />
+//       </div>
+//     );
+//   }
+// }
+
+// const mapDispatchToProps = (dispatch) => bindActionCreators({}, dispatch);
+
+// export default connect(null, mapDispatchToProps)(MainLayout);
+
+
 import React from "react";
 import { bindActionCreators } from "redux";
 import { connect } from "react-redux";
@@ -10,19 +45,20 @@ class MainLayout extends React.Component {
   componentDidMount() {}
 
   render() {
+    const isSettingsRoute = window.location.pathname === "/settings";
     return (
-      <div className="min-h-screen h-screen w-full flex flex-col bg-gray-100">
-        <VoiceAssistant/>
-        {window.location.pathname !== "/settings" ? (
+      <div className="min-h-screen h-[100vh] w-full flex flex-col bg-gray-100">
+        <VoiceAssistant />
+        {!isSettingsRoute ? (
           <Header className="flex-none" style={{ height: "10%" }} />
         ) : null}
         <main
           className="w-full overflow-auto scrollbar-hide"
-          style={{ height: "84%" }}
+          style={{ height: isSettingsRoute ? "94%" : "84%" }}
         >
           {this.props.children}
         </main>
-        <BottomNavigation className="flex-none"  />
+        <BottomNavigation className="flex-none" />
       </div>
     );
   }
